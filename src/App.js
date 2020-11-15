a06415471ef4a918857d6ed951963491de2b09e6
A Thomas Bianchi <atbianchi@gmail.com>
2020-11-15 11:30:47 -0500

    interview question

--- src/App.js
+++ src/App.js
@@ -1,13 +1,62 @@
 import React from 'react';
 
+
+
 export default class App extends React.Component {
-    // constructor(props) {
-    //     super(props);
-    // }
+    constructor(props) {
+        super(props);
+        this.changeDisplay = this.changeDisplay.bind(this);
+        this.incrementIndex = this.incrementIndex.bind(this);
+        this.decrementIndex = this.decrementIndex.bind(this);
+        this.state = {
+            cards: [
+                {
+                    country: "England",
+                    capital: "London",
+                },
+                {
+                    country: 'Canada',
+                    capital: 'Ottawa'
+                }
+            ],
+            showCountry: false,
+            index: 1,
+        }
+    }
+
+    changeDisplay() {
+        this.setState({
+            showCountry: !this.state.showCountry,
+        })
+    }
+
+    incrementIndex() {
+        if (this.state.index === this.state.cards.length - 1) {
+            return;
+        }
+        let newindex = this.state.index + 1;
+        this.setState({
+            index: newindex,
+        });
+    }
+
+    decrementIndex() {
+        if (this.state.index === 0) return;
+        let newindex = this.state.index - 1;
+        this.setState({
+            index: newindex,
+        });
+    }
 
     render() {
+        const index = this.state.index;
+        const display = this.state.showCountry ? this.state.cards[index].country : this.state.cards[index].capital;
         return (
-            <div>Hi</div>
+            <div className="card-container">
+                <div className="left-arrow" onClick={this.decrementIndex}>Previous card</div>
+                <div className="card" onClick={this.changeDisplay}> {display}</ div>
+                <div className="right-arrow" onClick={this.incrementIndex}>Next card</div>
+            </div>
         );
     }
 }
